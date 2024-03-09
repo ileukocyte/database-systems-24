d14123a38e8e162f0614e557f00f28e0d00b3df4
ileukocyte <alex.ilakeful@gmail.com>
2024-03-09 01:28:15 +0100

    dependencies updated + version bumped to 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,37 +1,37 @@
-val ktor_version: String by project
-val exposed_version: String by project
+val ktorVersion: String by project
+val exposedVersion: String by project
 
 plugins {
-    kotlin("jvm") version "1.9.22"
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("jvm") version "1.9.23"
+    kotlin("plugin.serialization") version "1.9.23"
 
-    id("io.ktor.plugin") version "2.3.8"
+    id("io.ktor.plugin") version "2.3.9"
 }
 
 group = "io.ileukocyte"
-version = "1.0.3"
+version = "2.0.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation(ktor("server-core-jvm", "2.3.8"))
-    implementation(ktor("server-netty-jvm", "2.3.8"))
+    implementation(ktor("server-core-jvm"))
+    implementation(ktor("server-netty-jvm"))
     implementation(kotlinx("serialization-json", "1.6.3"))
 
-    implementation("org.postgresql:postgresql:42.7.1")
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.postgresql:postgresql:42.7.2")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 
-    implementation("ch.qos.logback:logback-classic:1.4.14")
+    implementation("ch.qos.logback:logback-classic:1.5.3")
 
     testImplementation(kotlin("test"))
 }
 
+fun ktor(module: String, version: String = ktorVersion) = "io.ktor:ktor-$module:$version"
 fun kotlinx(module: String, version: String) = "org.jetbrains.kotlinx:kotlinx-$module:$version"
-fun ktor(module: String, version: String) = "io.ktor:ktor-$module:$version"
 
 tasks.test {
     useJUnitPlatform()
